3014c0e6e6b39c51a163edaa4c95d2eea8d9db1c
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-08-16 18:52:25 -0400

    added views and jsx files

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,8 +4,10 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('index')
+  // res.send(Bread)
 })
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
