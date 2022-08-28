254118336a3922da339b65e6508854d7b6d00737
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-08-28 10:20:42 -0400

    BAKER DRop down list

--- views/show.jsx
+++ views/show.jsx
@@ -16,11 +16,12 @@ function Show ({bread, index}) {
             }
             have gluten.
         </p>
-        <img src={bread.image} alt={bread.name} />
 
-        
+        <p>Baked by {bread.baker}</p>
+
+        <img src={bread.image} alt={bread.name}/>
+          
         <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
-
 
         <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
            <input type='submit' value="DELETE"/>
